9240675419af8e952fcbe86542023fd749ddcf55
asisthapa <asisthapa81@gmail.com>
2020-10-30 08:56:32 -0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+ $ conn=mysql_connect(" local host","id15248751_asisthapa","prajeGm*j\(0}rb2","id15248751_login",")
+if(!$conn){
+	echo "Not connected".......;
+	} else{
+		echo"connection successfully....."; 
+	}
+?>
